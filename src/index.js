c7687500d1d720697c95b7d06bde886994f480ed
Kim Brannian <brannian@gmail.com>
2021-11-26 21:10:48 -0800

    add files form previous repo with a broken environment

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import  ExchangeRateAPI from './js/ExchangeAPI.js';
+
+
+
+function clearFields() {
+  $('#amount').val("");
+  $('#showErrors').text("");
+  $('.showConversion').text("");
+}
+
+function entryError(usDollars,currency) {
+  if (NaN(usDollars) === true) {
+    $("#showErrors1").text(`"Error: please enter just a number, no $"`);
+  } 
+  if (currency === !currency) {
+    $('#showErrors2').text("Error: currency selected is not available");
+  }
+}
+
+$(document).ready(function() {
+  $('#form-convert').submit(function(event) {
+    event.preventDefault();
+    let usDollars = $("#amount").val();
+    let currency = $("#pickCurrency").val();
+    clearFields();
+    let promise = ExchangeRateAPI.getRate(currency);
+    promise.then(function(response) {
+      const result = JSON.parse(response); 
+      function conversion() {
+        return result.conversion_rate * usDollars;
+      }
+      $('#showConversion').text(`$${usDollars} USD converts to $${conversion()} ${currency}`);
+    }, function(error) {
+      $("#showError").text(`There was an error processing your request: ${error}`);
+    });
+    entryError();
+  });
+});
+
---
+++ src/js/ExchangeAPI.js
@@ -0,0 +1,17 @@
+export default class ExchangeRateAPI {
+  static getRate(currency) {
+    return new Promise(function(resolve,reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
